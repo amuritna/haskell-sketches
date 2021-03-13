f3fdece8cb1bde5148da1d9e017d54d2c22b67b5
Emmanuella Rumanti <amuritna@gmail.com>
2021-03-13 07:32:11 +0700

    Update 01_integers-and-recursions.hs

--- 01_integers-and-recursions.hs
+++ 01_integers-and-recursions.hs
@@ -16,7 +16,7 @@ rowOfOnes n
 -- rowOfOnes 1
 -- 1
 
-
+------------
 
 -- PANGKAT
 -- DEFINISI DAN SPESIFIKASI
@@ -36,7 +36,7 @@ pow a b
 -- pow 2 3
 -- 8 
 
-
+------------
 
 -- DECREASING DIGITS
 -- DEFINISI DAN SPESIFIKASI
@@ -54,7 +54,7 @@ decDigits n
 -- decDigits 5
 -- 54321
 
-
+---------
 
 -- LENGTH IN DIGTS
 -- DEFINISI DAN SPESIFIKASI
@@ -73,4 +73,102 @@ digLen x
 -- 5
 -- digLen 123456789
 -- 9
+
+---------
+
+-- CARI KELIPATAN
+-- DEFINISI DAN SPESIFIKASI
+nbKelipatanX :: Int -> Int -> Int -> Int
+{- Berdasarkan input M, N, dan X, mencari banyak kemunculan kelipatan x pada [m, n]. -}
+
+-- REALISASI
+nbKelipatanX m n x
+    | (m == n) =
+        if (mod n x == 0) then 0
+        else 1
+    | otherwise =
+        if (mod n x == 0) then (1 + (nbKelipatanX m (n - 1) x))
+        else (nbKelipatanX m (n - 1) x)
+
+-- APLIKASI
+-- nbKelipatanX 5 10 3
+-- 2
+-- nbKelipatanX 2 8 2
+-- 4
+-- nbKelipatanX 5 5 3
+-- 0
+
+---------
+
+-- PENJUMLAHAN SEMUA ELEMEN DALAM LIST
+-- DEFINISI DAN SPESIFIKASI
+sumInts :: [Int] -> Int
+{- Menjumlahkan semua elemen yang ada di list L. -}
+
+-- REALISASI
+sumInts l
+    | isEmpty l = 0 -- basis
+    | isOneElmt l = head l -- basis
+    | otherwise = head l + sumInts (tail l) --rekurens
+    
+-- APLIKASI
+-- sumInts [1, 2, 3, 4]
+-- 10
+-- sumInts [2, 2, 2, 2]
+-- 8
+ 
+---------
+ 
+-- GABUNG LIST (kalau yg ini sudah lolos autograder 100%)
+-- DEFINISI DAN SPESIFIKASI
+gabungList :: [Int] -> [Int] -> [Int]
+{- Menggabungkan dua list. -}
+
+-- REALISASI
+inList :: Int -> [Int] -> [Int] 
+inList new lst
+	| isEmpty lst = konso new lst 
+	| (new <= head lst) = konso new lst 
+	| otherwise =
+		konso (head lst) (inList new (tail lst))
+		
+gabungList l1 l2
+	| isEmpty l1 = l2
+	| otherwise =
+		gabungList (tail l1) (inList (head l1) l2)
+
+---------
+
+-- FILTER GANJIL (lolos autograder 100%)
+-- DEFINISI DAN SPESIFIKASI
+filterGanjil :: [Int] -> [Int]
+{- Hanya mengembalikan elemen list yang ganjil, sebagai list. -}
+
+-- REALISASI
+filterGanjil li
+	| isEmpty li = []
+    | (mod (head li) 2 == 1) = konso (head li) (filterGanjil (tail li))
+	| otherwise = filterGanjil (tail li)
+
+---------
+
+-- IS MEMBER (lolos autograder 100% juga hore)
+-- DEFINISI DAN SPESIFIKASI
+isMember :: [Int] -> Int -> Bool
+{- Berdasarkan input list L dan integer N, mengembalikan apakah N ada dalam L. -}
+
+-- REALISASI
+isMember l x
+	| isEmpty l = False
+	| isOneElmt l = ((head l) == x)
+	| otherwise =
+		if ((head l) == x) then True
+		else isMember (tail l) x
+
+
+
+
+
+
+
 
